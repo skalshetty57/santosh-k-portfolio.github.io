fb78ac5d81f13dc0eb8da3616efd1a1e798c6907
skalshetty57 <74254221+skalshetty57@users.noreply.github.com>
2021-07-09 18:54:21 +0530

    Add files via upload

--- projectCovidWatchJs.js
+++ projectCovidWatchJs.js
@@ -134,7 +134,7 @@ function changeMap(clickerId){
     let map=document.getElementById("map-state");
     let mapCircle=document.querySelectorAll("circle");
     // console.log(mapCircle);
-    let n=36;
+    let n=39;
     let mx=Math.max.apply(null,arr);
     let divider1=40.00/mx;
     let divider2=25.0/(0.4*mx);
@@ -144,7 +144,9 @@ function changeMap(clickerId){
     circleLegends[0].innerHTML=(Math.round(mx/10000)/100)+"L";
     circleLegends[1].innerHTML=(Math.round(mx*0.4/1000)/100)+"L";
     circleLegends[2].innerHTML=(Math.round(mx/1000)/100)+"L";
-    
+    circleLegends[0].setAttribute("fill",fillColor);
+    circleLegends[1].setAttribute("fill",fillColor);
+    circleLegends[2].setAttribute("fill",fillColor);
     
 
     for(let i=0;i<n;i++){
@@ -158,9 +160,12 @@ function changeMap(clickerId){
     map.setAttribute("stroke",mapColor);
     
     for(let i=0;i<n;i++){
-        mapCircle[i].setAttribute("fill",fillColor);
+        
         mapCircle[i].setAttribute("stroke",fillColor);
-        mapCircle[i].setAttribute("r",arr2[i]);
+        if(i<36){
+            mapCircle[i].setAttribute("fill",fillColor);
+            mapCircle[i].setAttribute("r",arr2[i]);
+        }
     }
     // console.log(states);
     // console.log(Math.max.apply(null,arr));
@@ -233,14 +238,20 @@ function hideResult(){
 }
 function displayResult(attri){
     // console.log(attri);
-    document.getElementById("state-result-name").innerHTML=attri;
+    
     let idx=states.indexOf(attri);
     // console.log(idx);
-    document.getElementById("state-confirmed").innerHTML=confirmed[idx];
-    document.getElementById("state-active").innerHTML=active[idx];
-    document.getElementById("state-recovered").innerHTML=discharged[idx];
-    document.getElementById("state-deceased").innerHTML=deceased[idx];
-
+    if(idx>=0){
+        document.getElementById("state-result-name").innerHTML=attri;
+    document.getElementById("state-confirmed").innerHTML=
+    numberWithCommas(confirmed[idx]);
+    document.getElementById("state-active").innerHTML=
+    numberWithCommas(active[idx]);
+    document.getElementById("state-recovered").innerHTML=
+    numberWithCommas(discharged[idx]);
+    document.getElementById("state-deceased").innerHTML=
+    numberWithCommas(deceased[idx]);
+    }
 
 }
 // let states=[];
@@ -248,3 +259,48 @@ function displayResult(attri){
 // let discharged=[];
 // let deceased=[];
 // let active=[];
+function changeColorScheme(){
+    let tt=document.getElementsByTagName("BODY")[0];
+    let tableEle=document.getElementsByTagName("TR");
+    let tableRows=tableEle.length;
+    console.log(tableEle);
+    if(tt.style.color=="rgb(22, 22, 37)"){
+        tt.style.color="mintcream";
+        tt.style.background="#161625";
+        document.getElementById("searcher").style.background="#1e1e30";
+        for(let j=0;j<5;j++){
+            tableEle[0]["cells"][j].style.background="rgb(68,77,85";
+        }
+        for(let i=0;i<tableRows;i++){
+            if(i%2==0){
+                tableEle[i].style.background="rgb(30,30,48";
+            }
+            tableEle[i]["cells"][0].style.background="rgb(68,77,85";
+        }
+        document.getElementById("view-mode-switch").innerHTML=
+        "<i class='far fa-sun'></i>";
+        document.getElementById("result-box").style.background=
+        "#161625";
+        
+    }
+    else{
+        tt.style.color="#161625";
+        tt.style.background="mintcream";
+        document.getElementById("searcher").style.background="mintcream";
+        for(let j=0;j<5;j++){
+            tableEle[0]["cells"][j].style.background="rgb(220,220,247";
+        }
+        for(let i=1;i<tableRows;i++){
+            if(i%2==0){
+                tableEle[i].style.background="rgb(220,220,247";
+            }
+            tableEle[i]["cells"][0].style.background="rgb(220,220,247";
+        }
+        document.getElementById("view-mode-switch").innerHTML=
+        "<i class='fas fa-moon'></i>";
+        document.getElementById("result-box").style.background=
+        "mintcream";
+    }
+    // console.log(tt);
+}
+// changeColorScheme();
